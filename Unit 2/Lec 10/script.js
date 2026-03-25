98c9bba74bb84a50c1dd83d83610945d5db5e90c
Shruti <shrutidolare27@gmail.com>
2026-03-25 13:59:26 +0530

    updated my code

---
+++ Unit 2/Lec 10/script.js
@@ -0,0 +1,31 @@
+// setTimeout
+
+// setTimeout(() => {
+//     console.log("Time is up!");
+// }, 3000);  // 3 sec delay
+
+// que 
+
+// console.log("First message");
+
+// setTimeout(() => {
+//     console.log("Second message (after 2 sec)");
+// }, 2000);
+
+// console.log("Third message");
+
+// setInterval 
+
+// setInterval(() => {
+//     console.log("Remainder: Drink water");
+// }, 5000);  // every 5 sec
+
+// que 
+
+console.log("Starting the interval...");
+
+setInterval(() => {
+    console.log("Remainder: Drink water");
+}, 2000);  // every 2 sec
+
+console.log("Interval has been set.");
