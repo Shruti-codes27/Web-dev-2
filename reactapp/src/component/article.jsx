8bde19aed167e8767b68643a1b5fb238995c5d63
Shruti <shrutidolare27@gmail.com>
2026-04-22 09:39:19 +0530

    updated code

--- reactapp/src/component/article.jsx
+++ reactapp/src/component/article.jsx
@@ -1,14 +1,50 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import ArtStyle from "../css/Article.module.css"
 // import styled from "styled-components"
 import styled from "@emotion/styled"
 import { css } from '@emotion/react'
+import axios from "axios"
 
 
 function Article(props) {
     const[count,setCount]=useState(0)
     const[age,setAge]=useState(0)
-    const[name,setName]=useState("")
+    const[name,setName]=useState("Alex")
+
+    useEffect(()=>{
+      console.log("Component Mounted")
+      return ()=>{
+        console.log("Component Unmounted")
+      }
+    },[ name])
+
+    useState(()=>{
+      // const fetchData=async()=>{
+      //   try{
+      //     const response=await fetch("https://dummyjson.com/products/222",{
+      //       method:"GET"
+      //     })
+      //     const data=await response.json()
+      //     console.log(data)
+      //   }catch(error){
+      //     console.log(error)
+      //   }
+      // }
+
+      const fetchData=async()=>{
+        try{
+          const response=await axios.get("https://dummyjson.com/products")
+          console.log(response.data)
+        }catch(error){
+          console.log(error)
+        }
+      }
+
+      fetchData()
+    },[])
+
+    
+
 
     const handleIncrement=()=>{
         setCount(count+1)
@@ -21,6 +57,8 @@ function Article(props) {
       <h3 style={{backgroundColor:"red"}}>{props.data}</h3>
       <h2>{count}</h2>
       <button className={ArtStyle.btn} onClick={handleIncrement}>Increment</button>
+      <h2>{name}</h2>
+      <button className='bg-yellow-900' onClick={()=>setName("John")}>Change Name</button>
       <Button>Click Me!</Button>
       <Button red >Click Again</Button>
       <button css={{backgroundColor:"green",color:"white",borderRadius:"10px",height:"30px",width:"150px",'&:hover':{backgroundColor:"blue"}}}>Css Prop Button</button>
