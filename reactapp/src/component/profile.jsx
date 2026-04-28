3e619320f0364529b8736d6230e9efbf9647b9d9
Shruti <shrutidolare27@gmail.com>
2026-04-28 12:30:46 +0530

    lec 33

---
+++ reactapp/src/component/profile.jsx
@@ -0,0 +1,15 @@
+import react from "react"
+import {useParams} from "react-router-dom"
+
+function Profile() {
+
+    const {username}=useParams()
+
+  return (
+    <div>
+      <h1>{username}</h1>
+    </div>
+  )
+}
+
+export default Profile
