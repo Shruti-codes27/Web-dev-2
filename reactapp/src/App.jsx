52a8a9b0c444441e75010be10d55acceb22e6ec7
Shruti <shrutidolare27@gmail.com>
2026-05-04 19:52:02 +0530

    updated code

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -5,18 +5,29 @@ import Article from './component/Article'
 import Homepage from './component/Homepage'
 import Contact from './component/Contact'
 import { Route, Routes } from 'react-router-dom'
+import Profile from './component/Profile'
+import { UserContext } from './UserContext'
+import Search from './component/Search'
+import Product from './component/Product'
 
 function App() {
   const[data,setData]=useState("Sample data")
+
+  const name="Alex"
   return (
     <>
 
-      <Routes>
-        <Route path="/article" element={<Article data={data} />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/" element={<><Homepage /><Menu/></>} />
-        <Route path="/profile/:username" element={<Profile />} />
-      </Routes>
+      <UserContext.Provider value={name}>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/menu" element={<Menu/>}/>
+          <Route path="/article" element={<Article/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/profile/:id" element={<Profile/>}/>
+          <Route path='/search' element={<Search/>}/>
+          <Route path="/product" element={<Product/>}/>
+        </Routes>
+      </UserContext.Provider>
       
     </>
   )
